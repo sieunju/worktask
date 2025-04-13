357035f7aa6558f5c707a6400e71b70373020f32
sieunju <j.sieun73@gmail.com>
2025-04-13 23:33:45 +0900

    fix: 누락된 DiffUtil 조건문 수정

--- presentation/src/main/java/com/hmju/presentation/util/CommonDiffUtil.kt
+++ presentation/src/main/java/com/hmju/presentation/util/CommonDiffUtil.kt
@@ -2,10 +2,12 @@ package com.hmju.presentation.util
 
 import androidx.recyclerview.widget.DiffUtil
 import com.hmju.presentation.models.BaseUiModel
+import com.hmju.presentation.models.ProductGridUiModel
 import com.hmju.presentation.models.ProductHorizontalUiModel
 import com.hmju.presentation.models.ProductVerticalUiModel
 import com.hmju.presentation.models.SectionGridUiModel
 import com.hmju.presentation.models.SectionHorizontalUiModel
+import com.hmju.presentation.models.SectionVerticalUiModel
 
 /**
  * Description : RecyclerView DiffUtilClass
@@ -26,12 +28,20 @@ class CommonDiffUtil : DiffUtil.ItemCallback<BaseUiModel>() {
             newItem is ProductVerticalUiModel
         ) {
             oldItem.id == newItem.id
+        } else if (oldItem is ProductGridUiModel &&
+            newItem is ProductGridUiModel
+        ) {
+            oldItem.id == newItem.id
         } else if (oldItem is SectionHorizontalUiModel &&
             newItem is SectionHorizontalUiModel
         ) {
             oldItem.id == newItem.id
         } else if (oldItem is SectionGridUiModel &&
             newItem is SectionGridUiModel
+        ) {
+            oldItem.id == newItem.id
+        } else if (oldItem is SectionVerticalUiModel &&
+            newItem is SectionVerticalUiModel
         ) {
             oldItem.id == newItem.id
         } else {
@@ -51,12 +61,20 @@ class CommonDiffUtil : DiffUtil.ItemCallback<BaseUiModel>() {
             newItem is ProductVerticalUiModel
         ) {
             oldItem == newItem
+        } else if (oldItem is ProductGridUiModel &&
+            newItem is ProductGridUiModel
+        ) {
+            oldItem == newItem
         } else if (oldItem is SectionHorizontalUiModel &&
             newItem is SectionHorizontalUiModel
         ) {
             oldItem.id == newItem.id
         } else if (oldItem is SectionGridUiModel &&
             newItem is SectionGridUiModel
+        ) {
+            oldItem.id == newItem.id
+        } else if (oldItem is SectionVerticalUiModel &&
+            newItem is SectionVerticalUiModel
         ) {
             oldItem.id == newItem.id
         } else {
