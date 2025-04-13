51a5a9bac6de7245ee3a5e150c4d4c48587d7097
sieunju <j.sieun73@gmail.com>
2025-04-13 18:34:16 +0900

    feat: 메인 화면 전용 UseCase 추가

---
+++ domain/src/main/java/com/hmju/domain/models/SectionWithProductData.kt
@@ -0,0 +1,23 @@
+package com.hmju.domain.models
+
+import com.hmju.domain.enums.ProductViewType
+
+/**
+ * Description : 세션 With 상품 리스트 데이터 모델
+ *
+ * Created by juhongmin on 2025. 4. 13.
+ */
+data class SectionWithProductData(
+    val title: String,
+    val productType: ProductViewType,
+    val list: List<ProductData> = listOf()
+) {
+    constructor(
+        section: SectionData,
+        list: List<ProductData>
+    ) : this(
+        title = section.title,
+        productType = section.productType,
+        list = list
+    )
+}
