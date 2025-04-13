e21463545a0009c1d51ff3b9ed3f78b335d359ac
sieunju <j.sieun73@gmail.com>
2025-04-13 18:52:01 +0900

    feat: Timber 로그 설정

--- app/src/main/java/com/hmju/worktask/MainApplication.kt
+++ app/src/main/java/com/hmju/worktask/MainApplication.kt
@@ -2,6 +2,7 @@ package com.hmju.worktask
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 /**
  * Description : MainApplication
@@ -13,5 +14,22 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        Timber.plant(object : Timber.DebugTree() {
+
+            override fun createStackElementTag(element: StackTraceElement): String {
+                val str = StringBuilder("Timber")
+                try {
+                    str.append(" ")
+                    str.append(element.methodName.substringAfterLast("."))
+                } catch (_: Exception) {
+
+                }
+                return str.toString()
+            }
+        })
     }
 }
